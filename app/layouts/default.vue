99df7c33f712cd69e7166cd0edac1613e781aeaa
Miguelete4 <miguelguajardo189@gmail.com>
2026-06-25 20:33:48 -0400

    nav diseño

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,16 +5,30 @@
 </script>
 
 <template>
-    <div>
-        <header class=""> </header>
+    <div class="flex flex-col min-h-screen justify-center mt-auto">
+        <header class="top-0 bg-white shadow-md">
 
-        <nav class=""> </nav>
+            <nav class="sticky top-0 bg-white ">
+                <div class="">
+                    <a class="text-gray-800 hover:text-blue-500" href="/">Inicio</a>
+                    <a class="text-gray-800 hover:text-blue-500" href="/eventos">Eventos</a>
+
+                </div>
+
+                <div class="">Inciar sesion</div>
+            </nav>
+
+        </header>
 
         <main>
             <slot />
         </main>
 
-        <footer class=""> </footer>
+        <footer class="bg-gray-800 text-white py-6">
+            <div class="mx-auto px-4">
+                <p></p>
+            </div>
+        </footer>
 
     </div>
 </template>
